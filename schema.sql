277054569f991573b799ea873fd51a2392277a29
Augusto <jesus_augga_99@icloud.com>
2022-05-31 08:57:26 -0500

    add index

--- schema.sql
+++ schema.sql
@@ -1,10 +1,12 @@
 /* Database schema to keep the structure of entire database. */
 
 CREATE TABLE animals (
-    id integer,
+    id integer GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     name text,
     date_of_birth date,
     escape_attempts integer,
     neutered boolean,
     weight_kg numeric
 );
+
+CREATE UNIQUE INDEX animals_pkey ON animals(id int4_ops);
