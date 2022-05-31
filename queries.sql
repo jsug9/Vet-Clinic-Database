07b72832da169825d2927577d45b2ae3b3430962
Augusto <jesus_augga_99@icloud.com>
2022-05-31 08:47:59 -0500

    fix a typo

--- queries.sql
+++ queries.sql
@@ -6,5 +6,5 @@ SELECT name FROM animals WHERE neutered = TRUE AND escape_attempts < 3;
 SELECT date_of_birth FROM animals WHERE name in ('Agumon', 'Pikachu');
 SELECT name, escape_attempts FROM animals WHERE weight_kg > 10.5;
 SELECT * FROM animals WHERE neutered = TRUE;
-SELECT * from animals WHERE name != 'Gabumon';
+SELECT * FROM animals WHERE name != 'Gabumon';
 SELECT * FROM animals WHERE weight_kg >= 10.4 AND weight_kg <= 17.3; 
