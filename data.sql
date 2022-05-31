2a20ce80a5cedf8a8260bc7e33bb342c491438e0
Augusto <jesus_augga_99@icloud.com>
2022-05-31 15:51:25 -0500

    move transactions to queries file

--- data.sql
+++ data.sql
@@ -16,29 +16,3 @@ VALUES
 (E'Boarmon',E'2005-06-07',7,TRUE,20.4),
 (E'Blossom',E'1998-10-13',3,TRUE,17),
 (E'Ditto',E'2022-05-14',4,TRUE,22);
-
-BEGIN;
-UPDATE "public"."animals"
-SET species = 'unspecified';
-ROLLBACK;
-
-BEGIN;
-UPDATE "public"."animals"
-SET species = 'digimon'
-WHERE name LIKE '%mon'; 
-UPDATE "public"."animals"
-SET species = 'pokemon'
-WHERE species is NULL; 
-COMMIT;
-
-BEGIN;
-DELETE FROM animals WHERE date_of_birth > '2022-01-01';
-SAVEPOINT delete_by_date;
-UPDATE animals
-SET weight_kg = weight_kg * -1
-ROLLBACK TO delete_by_date;
-UPDATE animals
-SET weight_kg = weight_kg * -1
-WHERE weight_kg < 0;
-SELECT * from animals;
-COMMIT;
--- queries.sql
+++ queries.sql
@@ -8,3 +8,41 @@ SELECT name, escape_attempts FROM animals WHERE weight_kg > 10.5;
 SELECT * FROM animals WHERE neutered = TRUE;
 SELECT * FROM animals WHERE name != 'Gabumon';
 SELECT * FROM animals WHERE weight_kg >= 10.4 AND weight_kg <= 17.3; 
+
+BEGIN;
+UPDATE "public"."animals"
+SET species = 'unspecified';
+ROLLBACK;
+
+BEGIN;
+UPDATE "public"."animals"
+SET species = 'digimon'
+WHERE name LIKE '%mon'; 
+UPDATE "public"."animals"
+SET species = 'pokemon'
+WHERE species is NULL; 
+COMMIT;
+
+-- TRANSACTIONS
+-- Delete all animals born after Jan 1st, 2022.
+BEGIN;
+DELETE FROM animals WHERE date_of_birth > '2022-01-01';
+
+-- Create a savepoint for the transaction.
+SAVEPOINT delete_by_date;
+
+-- Update all animals' weight to be their weight multiplied by -1.
+UPDATE animals
+SET weight_kg = weight_kg * -1
+
+-- Rollback to the savepoint
+ROLLBACK TO delete_by_date;
+
+-- Update all animals' weights that are negative to be their weight multiplied by -1.
+UPDATE animals
+SET weight_kg = weight_kg * -1
+WHERE weight_kg < 0;
+
+-- Commit transaction
+SELECT * from animals;
+COMMIT;
