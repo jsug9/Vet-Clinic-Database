5b41dd937a3cdb2458ad3664461059549af16e77
Augusto <jesus_augga_99@icloud.com>
2022-06-01 14:04:08 -0500

    add column owneer to animals

--- schema.sql
+++ schema.sql
@@ -31,3 +31,17 @@ CREATE TABLE species (
 );
 
 CREATE UNIQUE INDEX species_pkey ON species(id int4_ops);
+
+-- Delete species from animals table.
+ALTER TABLE animals 
+    DROP COLUMN species;
+
+-- Add column species_id which is a foreign key referencing species table
+ALTER TABLE animals
+    ADD species_id INT,
+    ADD CONSTRAINT species_id FOREIGN KEY(species_id) REFERENCES species(id);
+
+-- Add column owner_id which is a foreign key referencing the owners table
+ALTER TABLE animals
+    ADD owner_id INT,
+    ADD CONSTRAINT owner_id FOREIGN KEY(owner_id) REFERENCES owners(id);
