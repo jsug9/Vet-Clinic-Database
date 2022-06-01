7293e70626ca1d4ff25d7ab83fdfb29aa5f45ed6
Augusto <jesus_augga_99@icloud.com>
2022-06-01 17:38:52 -0500

    add visists with vet without specialization in an animal specie

--- queries.sql
+++ queries.sql
@@ -149,5 +149,11 @@ SELECT * FROM visits
 	LIMIT 1;
 
 -- How many visits were with a vet that did not specialize in that animal's species?
+SELECT COUNT(*)
+	FROM visits
+	JOIN animals ON animals.id = visits.animals_id
+	JOIN vets ON vets.id = visits.vets_id
+	JOIN specializations ON specializations.vets_id = visits.vets_id
+	WHERE animals.species_id != specializations.species_id;
 
 -- What specialty should Maisy Smith consider getting? Look for the species she gets the most.
