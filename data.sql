fa3c4ebc47a8b5a3aa7eeb47b6e8ffdafef6c0ba
Augusto <jesus_augga_99@icloud.com>
2022-06-06 15:45:33 -0500

    add owners

--- data.sql
+++ data.sql
@@ -82,3 +82,6 @@ INSERT INTO public.visits (animals_id, vets_id, date_of_visit) VALUES
 -- Add visits
 INSERT INTO visits(animals_id, vets_id, date_of_visit) 
   SELECT * FROM (SELECT id FROM animals) animal_ids, (SELECT id FROM vets) vets_ids, generate_series('1980-01-01'::timestamp, '2021-01-01', '4 hours') visit_timestamp;
+
+-- Add owners
+INSERT INTO owners (full_name, email) select 'Owner ' || generate_series(1,2500000), 'owner_' || generate_series(1,2500000) || '@mail.com';
