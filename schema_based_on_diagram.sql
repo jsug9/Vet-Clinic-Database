7935f54044ca184854ceb2d590a5712016dac6b5
Augusto <jesus_augga_99@icloud.com>
2022-06-07 12:24:42 -0500

    add invoices table

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -7,3 +7,27 @@ CREATE TABLE patients (
 );
 
 CREATE UNIQUE INDEX patients_pkey ON patients(id int4_ops);
+
+-- Add invoices table
+CREATE TABLE invoices (
+    id integer GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    total_amount numeric,
+    generated_at timestamp without time zone,
+    payed_at timestamp without time zone,
+    "medical_history_
+id" integer REFERENCES medical_histories(id)
+);
+
+CREATE UNIQUE INDEX invoices_pkey ON invoices(id int4_ops);
+CREATE INDEX "medical_history_
+id_index" ON invoices("medical_history_
+id" int4_ops);
+
+-- Add medical histories table
+
+
+-- Add invoice items table
+
+
+-- Add treatments table
+
